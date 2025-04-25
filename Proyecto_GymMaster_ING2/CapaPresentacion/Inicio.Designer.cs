c3b0e04a028c66108531bc53fabaa92f0cefe810
Priscila Rodríguez <rodriguezpris10@gmail.com>
2025-04-25 13:14:50 -0300

    Formulario de login - Capa presentacion

---
+++ Proyecto_GymMaster_ING2/CapaPresentacion/Inicio.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace CapaPresentacion
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.titulo = new System.Windows.Forms.Label();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxDni = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxContraseña = new System.Windows.Forms.TextBox();
+            this.BIngresar = new FontAwesome.Sharp.IconButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BCancelar = new FontAwesome.Sharp.IconButton();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.BackColor = System.Drawing.Color.Black;
+            this.titulo.Font = new System.Drawing.Font("Britannic Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titulo.ForeColor = System.Drawing.Color.OrangeRed;
+            this.titulo.Location = new System.Drawing.Point(760, 369);
+            this.titulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(469, 89);
+            this.titulo.TabIndex = 3;
+            this.titulo.Text = "GYMMASTER";
+            this.titulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.iconPictureBox1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Dumbbell;
+            this.iconPictureBox1.IconColor = System.Drawing.Color.OrangeRed;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 180;
+            this.iconPictureBox1.Location = new System.Drawing.Point(803, 231);
+            this.iconPictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(387, 180);
+            this.iconPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.iconPictureBox1.TabIndex = 4;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.OrangeRed;
+            this.label2.Font = new System.Drawing.Font("Microsoft PhagsPa", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(187, 148);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(293, 57);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Bienvenido";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.OrangeRed;
+            this.label3.Font = new System.Drawing.Font("Microsoft PhagsPa", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(153, 198);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(360, 57);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Inicie sesión para continuar";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxDni
+            // 
+            this.textBoxDni.BackColor = System.Drawing.Color.White;
+            this.textBoxDni.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDni.Location = new System.Drawing.Point(113, 324);
+            this.textBoxDni.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxDni.Name = "textBoxDni";
+            this.textBoxDni.Size = new System.Drawing.Size(439, 34);
+            this.textBoxDni.TabIndex = 7;
+            this.textBoxDni.TextChanged += new System.EventHandler(this.TextBoxDni_TextChanged);
+            this.textBoxDni.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxDni_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.OrangeRed;
+            this.label4.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(100, 281);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 39);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "DNI";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.OrangeRed;
+            this.label5.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(108, 391);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 39);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Contraseña";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxContraseña
+            // 
+            this.textBoxContraseña.BackColor = System.Drawing.Color.White;
+            this.textBoxContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxContraseña.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBoxContraseña.Location = new System.Drawing.Point(113, 434);
+            this.textBoxContraseña.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxContraseña.Name = "textBoxContraseña";
+            this.textBoxContraseña.PasswordChar = '*';
+            this.textBoxContraseña.Size = new System.Drawing.Size(439, 34);
+            this.textBoxContraseña.TabIndex = 9;
+            this.textBoxContraseña.TextChanged += new System.EventHandler(this.TextBoxContraseña_TextChanged);
+            // 
+            // BIngresar
+            // 
+            this.BIngresar.BackColor = System.Drawing.Color.DarkGreen;
+            this.BIngresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BIngresar.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.BIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BIngresar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BIngresar.ForeColor = System.Drawing.Color.White;
+            this.BIngresar.IconChar = FontAwesome.Sharp.IconChar.DoorOpen;
+            this.BIngresar.IconColor = System.Drawing.Color.White;
+            this.BIngresar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BIngresar.IconSize = 30;
+            this.BIngresar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BIngresar.Location = new System.Drawing.Point(136, 514);
+            this.BIngresar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BIngresar.Name = "BIngresar";
+            this.BIngresar.Size = new System.Drawing.Size(154, 59);
+            this.BIngresar.TabIndex = 11;
+            this.BIngresar.Text = "INGRESAR";
+            this.BIngresar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BIngresar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BIngresar.UseVisualStyleBackColor = false;
+            this.BIngresar.Click += new System.EventHandler(this.BIngresar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.OrangeRed;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.BCancelar);
+            this.panel1.Controls.Add(this.BIngresar);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(667, 738);
+            this.panel1.TabIndex = 12;
+            // 
+            // BCancelar
+            // 
+            this.BCancelar.BackColor = System.Drawing.Color.Red;
+            this.BCancelar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BCancelar.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.BCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BCancelar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BCancelar.ForeColor = System.Drawing.Color.White;
+            this.BCancelar.IconChar = FontAwesome.Sharp.IconChar.CircleXmark;
+            this.BCancelar.IconColor = System.Drawing.Color.White;
+            this.BCancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BCancelar.IconSize = 30;
+            this.BCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BCancelar.Location = new System.Drawing.Point(359, 514);
+            this.BCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BCancelar.Name = "BCancelar";
+            this.BCancelar.Size = new System.Drawing.Size(160, 59);
+            this.BCancelar.TabIndex = 13;
+            this.BCancelar.Text = "CANCELAR";
+            this.BCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BCancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BCancelar.UseVisualStyleBackColor = false;
+            this.BCancelar.Click += new System.EventHandler(this.BCancelar_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Inicio
+            // 
+            this.AcceptButton = this.BIngresar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1333, 738);
+            this.Controls.Add(this.textBoxContraseña);
+            this.Controls.Add(this.textBoxDni);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.titulo);
+            this.Controls.Add(this.iconPictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Inicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TransparencyKey = System.Drawing.Color.Gold;
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label titulo;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDni;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxContraseña;
+        private FontAwesome.Sharp.IconButton BIngresar;
+        private System.Windows.Forms.Panel panel1;
+        private FontAwesome.Sharp.IconButton BCancelar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Proyecto_GymMaster_ING2/CapaPresentacion/Inicio.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FontAwesome.Sharp;
+
+using CapaNegocio;
+using CapaEntidad;
+
+namespace CapaPresentacion
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void BIngresar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TextBoxDni_TextChanged(object sender, EventArgs e)
+        {
+            if(textBoxDni.Text.Length > 8)
+            {
+                // limita el campo a 8 digitos
+                textBoxDni.Text = textBoxDni.Text.Substring(0, 8);
+                textBoxDni.SelectionStart = textBoxDni.Text.Length; //coloca el cursor al final del texto
+            }
+        }
+
+        private void TextBoxDni_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //solo admite numeros
+            if(!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true; //ignora la tecla si no es numero o backspace
+            }
+        }
+
+        private void TextBoxContraseña_TextChanged(object sender, EventArgs e)
+        {
+            if(textBoxContraseña.Text.Length < 8)
+            {
+                errorProvider1.SetError(textBoxContraseña, "La contraseña debe tener al menos 8 caracteres.");
+            }
+            else
+            {
+                errorProvider1.SetError(textBoxContraseña, "");
+            }
+        }
+    }
+}
