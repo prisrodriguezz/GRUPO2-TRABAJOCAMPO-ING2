2859984c4a168c70581d1904325c8a61544563f8
Yago Ayala <yago.ayala.ius@gmail.com>
2025-04-25 17:59:48 -0300

    se agrega metodo Listar a datos

---
+++ Proyecto_GymMaster_ING2/CapaDatos/CD_usuario.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidad;
+
+namespace CapaDatos
+{
+    public class CD_usuario
+    {
+        public List<Usuario> Listar(){
+            List<Usuario> lista = new List<Usuario>();
+
+            using (SqlConnection conexion = new SqlConnection(Conexion.cadena)) {
+                try
+                {
+
+                    string query = "SELECT u.id_usuario, u.id_rol, r.descripcion AS rol_descripcion, u.nombre, u.email, u.telefono, u.dni, u.fecha_nacimiento, u.estado, u.contraseña, u.apellido " +
+                               "FROM Usuario u " +
+                               "JOIN Rol r ON u.id_rol = r.id_rol";
+                    SqlCommand cmd = new SqlCommand(query, conexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    conexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Usuario()
+                            {
+                                id_usuario = Convert.ToInt32(dr["id_usuario"]),
+                                id_rol = new Rol { id_rol = Convert.ToInt32(dr["id_rol"]),
+                                                   descripcion = dr["rol_descripcion"].ToString()},
+                                nombre = dr["nombre"].ToString(),
+                                email = dr["email"].ToString(),
+                                telefono = dr["telefono"].ToString(),
+                                dni = dr["dni"].ToString(),
+                                fecha_nacimiento = dr["fecha_nacimiento"].ToString(),
+                                estado = Convert.ToBoolean(dr["estado"]),
+                                contraseña = dr["contraseña"].ToString(),
+                                apellido = dr["apellido"].ToString()
+
+
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex) {
+                    Console.WriteLine("Error: " + ex.Message);
+                    //lista = new List<Usuario>();
+                }
+            }
+            return lista;
+        }
+
+    }
+}
