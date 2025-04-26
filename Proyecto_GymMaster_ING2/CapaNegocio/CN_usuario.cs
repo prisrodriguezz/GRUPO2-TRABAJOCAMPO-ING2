af82b20b63ddc23ce0c26db3173f3ca14040f9ce
Priscila Rodríguez <rodriguezpris10@gmail.com>
2025-04-26 14:45:58 -0300

    Capa Negocio - Usuario

---
+++ Proyecto_GymMaster_ING2/CapaNegocio/CN_usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CN_usuario
+    {
+        private CD_usuario objcd_usuario = new CD_usuario();
+
+        public List<Usuario> Listar() {
+            return objcd_usuario.Listar();
+        }
+    }
+}
