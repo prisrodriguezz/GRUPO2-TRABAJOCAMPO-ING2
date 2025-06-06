8331fec621e2f913f62375421b9974dd80609a28
Yago Ayala <yago.ayala.ius@gmail.com>
2025-06-06 19:17:09 -0300

    Se agrega Pago a capa Entidad

---
+++ Proyecto_GymMaster_ING2/CapaEntidad/Pago.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Pago
+    {
+        public int id_pago { get; set; }
+        public Usuario id_usuario { get; set; }
+        public Alumno id_alumno { get; set; }
+        public MedioPago id_medioPago { get; set; }
+        public string fecha { get; set; }
+        public decimal cantidad { get; set; }
+        public decimal total { get; set; }
+        public decimal recargo { get; set; }
+    }
+}
